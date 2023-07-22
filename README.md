991ef262f19423e6cc75d4cdbba5da23e89514cc
Joe <jrbevan537@gmail.com>
2023-07-22 18:24:33 +0200

    Create IIFE to render game board

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Tic-Tac-Toe
+
+A simple game of Tic Tac Toe, inspired by the Odin Project.
+
+The purpose of this project is to practise Modules, Factory Functions, closure and maintaining clean global namespace.
--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-const Gameboard = (() => {
-  let gameboard = ["", "", "", "", "", "", "", "", ""];
-
-  const createBoard = () => {};
+// Function to create the game board
+const makeGameboard = (() => {
+  const gameBoard = document.querySelector("#gameboard");
+  for (let i = 0; i <= 8; i++) {
+    let square = document.createElement("div");
+    square.classList.add("square");
+    gameBoard.appendChild(square);
+  }
 })();
