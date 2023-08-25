2c2eb99ac8a9bb0d2a4c50715fdbd34f2871d5b0
Joe <jrbevan537@gmail.com>
2023-08-25 12:03:34 +0200

    Add new game button

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // Gameboard object - IIFE module
 const makeGameboard = (() => {
   let boardArray = ["", "", "", "", "", "", "", "", ""];
-
   const displayBoard = () => {
     const gameBoard = document.querySelector("#gameboard");
+    gameBoard.innerHTML = "";
     boardArray.forEach((element) => {
       let square = document.createElement("div");
       square.classList.add("square");
@@ -11,50 +11,36 @@ const makeGameboard = (() => {
     });
   };
 
+  // Return the displayBoard method so it can be accessed outside of the object
   return {
     displayBoard,
   };
 })();
 
-makeGameboard.displayBoard();
-
-// const makeGameboard = (() => {
-//   const gameBoard = document.querySelector("#gameboard");
-//   const boardArray = ["X", "X", "X", "O", "O", "O", "X", "X", "X"];
-//   boardArray.forEach((element) => {
-//     let square = document.createElement("div");
-//     square.classList.add("square");
-//     gameBoard.appendChild(square);
-//   });
-//   console.log(boardArray);
-// })();
-
-// Player object - factory
-function Player(playerName, shape) {
-  const getName = () => playerName;
-  const getShape = () => shape;
-  return { getName, getShape };
-}
-
-// Game object
-const Game = () => {
-  // Create players and console log name and shape
-  const player1 = Player("Player 1", "X");
-  console.log(`${player1.getName()} plays ${player1.getShape()}`);
-
-  const player2 = Player("Player 2", "O");
-  console.log(`${player2.getName()} plays ${player2.getShape()}`);
-
-  let currentPlayerIndex = 0;
-
-  // Function to add marker to board on click.
-  // query selector to grab cells
-  // Change class on click
-
-  const addMarker = () => {
-    console.log("Add marker");
+const createPlayer = (name, mark) => {
+  return {
+    name,
+    mark,
   };
-  return addMarker;
 };
 
-Game.addMarker;
+const Game = (() => {
+  let players = [];
+  let currentPlayerIndex;
+  let gameOver;
+
+  const start = () => {
+    makeGameboard.displayBoard();
+    players = [createPlayer("Player 1", "X"), createPlayer("Player 2", "O")];
+    currentPlayerIndex = 0;
+    gameOver = false;
+  };
+  return {
+    start,
+  };
+})();
+
+const newGameButton = document.querySelector("#newGameButton");
+newGameButton.addEventListener("click", () => {
+  Game.start();
+});
